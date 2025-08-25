f5b3a08f77f8dde424abdc3ae3c40c9557a3e0d9
Lattn <lattnx@gmail.com>
2025-08-25 23:03:37 +0800

    feat: Set directive order for ip_filter

--- caddyfile.go
+++ caddyfile.go
@@ -14,6 +14,7 @@ import (
 func init() {
 	caddy.RegisterModule(&IPFilter{})
 	httpcaddyfile.RegisterHandlerDirective("ip_filter", parseCaddyfile)
+	httpcaddyfile.RegisterDirectiveOrder("ip_filter", httpcaddyfile.Before, "basic_auth")
 }
 
 // CaddyModule Return information of Caddy module
